54952cceea3734faf382246bde7f05161ae8db2a
Adrian <322735+adrian-schnell@users.noreply.github.com>
2021-11-14 14:48:25 +0100

    enable the command in the kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,33 +8,16 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
     protected $commands = [
-	    //
 	    UpdateVaultDataCommand::class,
     ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+         $schedule->command(UpdateVaultDataCommand::class, ['--max=100'])->everyFiveMinutes();
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
+    protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
 
