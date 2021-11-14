51b9da56fcb6e10e2d579ec9a3bbc7ba26d268fd
Adrian <322735+adrian-schnell@users.noreply.github.com>
2021-11-14 15:09:43 +0100

    add Fixed Interval Price migration & model

---
+++ app/Models/FixedIntervalPrice.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @mixin \Eloquent
+ * @property int     id
+ * @property string  priceFeedId
+ * @property float   activePrice
+ * @property float   nextPrice
+ * @property Carbon  timestamp
+ * @property string  validFromTimeString
+ * @property boolean isLive
+ */
+class FixedIntervalPrice extends Model
+{
+	protected $primaryKey = 'priceFeedId';
+	protected $table = 'fixed_interval_prices';
+	protected $casts = [
+		'timestamp' => 'timestamp',
+	];
+	protected $fillable = [
+		'priceFeedId',
+		'activePrice',
+		'nextPrice',
+		'timestamp',
+		'isLive',
+	];
+
+	public function getValidFromTimeStringAttribute(): string
+	{
+		return Carbon::parse($this->timestamp)->toDateTimeString();
+	}
+
+	public function getPriceFeedIdAttribute(): string
+	{
+		return $this->attributes['priceFeedId'];
+	}
+}
