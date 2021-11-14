33eb9bc410ee5223e7fdbba7ccc85db83c3a76d2
Adrian Schnell <322735+adrian-schnell@users.noreply.github.com>
2021-11-14 15:03:38 +0100

    update loan scheme command (#11)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,37 +2,22 @@
 
 namespace App\Console;
 
+use App\Console\Commands\UpdateLoanSchemeCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
     protected $commands = [
-        //
+	    UpdateLoanSchemeCommand::class,
     ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+         $schedule->call(UpdateLoanSchemeCommand::class)->daily();
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
+    protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
 
