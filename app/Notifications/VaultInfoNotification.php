945148cdcbdcedf200196a80275070ca3c964841
Adrian <322735+adrian-schnell@users.noreply.github.com>
2021-11-16 16:00:01 +0100

    add BaseNotification class

--- app/Notifications/VaultInfoNotification.php
+++ app/Notifications/VaultInfoNotification.php
@@ -2,42 +2,15 @@
 
 namespace App\Notifications;
 
-use App\Enum\CooldownTypes;
-use App\Enum\NotificationGatewayType;
-use App\Enum\QueueName;
 use App\Models\NotificationTrigger;
-use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use JetBrains\PhpStorm\ArrayShape;
 use NotificationChannels\Telegram\TelegramFile;
 
-class VaultInfoNotification extends Notification implements ShouldQueue
+class VaultInfoNotification extends BaseNotification implements ShouldQueue
 {
 	use Queueable;
-
-	public function __construct()
-	{
-
-	}
-
-	public function via(NotificationTrigger $notifiable): array
-	{
-		$methods = [];
-		if ($notifiable->hasGateway(NotificationGatewayType::TELEGRAM)
-			&& $notifiable->cooldown(CooldownTypes::TELEGRAM_NOTIFICATION)->passed()) {
-			$methods[] = NotificationGatewayType::TELEGRAM;
-		}
-		if ($notifiable->hasGateway(NotificationGatewayType::WEBHOOK)) {
-			$methods[] = NotificationGatewayType::WEBHOOK;
-		}
-		if ($notifiable->hasGateway(NotificationGatewayType::MAIL)) {
-			$methods[] = NotificationGatewayType::MAIL;
-		}
-
-		return $methods;
-	}
 
 	public function toTelegram(NotificationTrigger $notificationTrigger): TelegramFile
 	{
@@ -75,15 +48,5 @@ class VaultInfoNotification extends Notification implements ShouldQueue
 	public function toWebhook(NotificationTrigger $notifiable): void
 	{
 
-	}
-
-	#[ArrayShape(['telegram' => "string", 'mail' => "string", 'webhook' => "string"])]
-	public function viaQueues(): array
-	{
-		return [
-			'telegram' => QueueName::NOTIFICATION_TELEGRAM_QUEUE,
-			'mail'     => QueueName::NOTIFICATION_EMAIL_QUEUE,
-			'webhook'  => QueueName::NOTIFICATION_WEBHOOK_QUEUE,
-		];
 	}
 }
