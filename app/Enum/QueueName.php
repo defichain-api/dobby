fa22f6e5e380f4738a54080155d463e97fa2aeb9
Adrian <322735+adrian-schnell@users.noreply.github.com>
2021-11-20 18:58:41 +0100

    add env to horizon queue names

--- app/Enum/QueueName.php
+++ app/Enum/QueueName.php
@@ -9,4 +9,11 @@ class QueueName
 	const NOTIFICATION_WEBHOOK_QUEUE = 'notification_webhook';
 	const API_CALLS_QUEUE = 'api_calls';
 	const UPDATE_VAULTS_QUEUE = 'update_vaults';
+	const QUEUES_ALL = [
+		self::NOTIFICATION_TELEGRAM_QUEUE,
+		self::NOTIFICATION_EMAIL_QUEUE,
+		self::NOTIFICATION_WEBHOOK_QUEUE,
+		self::API_CALLS_QUEUE,
+		self::UPDATE_VAULTS_QUEUE,
+	];
 }
