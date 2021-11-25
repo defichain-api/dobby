a45692d8a557d347b8708a74cf9c19d4bb2c33ae
Adrian Schnell <322735+adrian-schnell@users.noreply.github.com>
2021-11-25 11:40:21 +0100

    Merge pull request #29
    * change decimal place for prices table
    
    * update prices every 5 min

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
 		$schedule->command(UpdateLoanSchemeCommand::class)
 			->daily();
 		$schedule->command(UpdateFixedIntervalPriceCommand::class)
-			->everyThirtyMinutes();
+			->everyFiveMinutes();
 		$schedule->command(PruneInactiveUsersCommand::class)
 			->weekly();
 	}
