35cd620a75ed3bdc85114ccc2e338d5f285eca55
Adrian <322735+adrian-schnell@users.noreply.github.com>
2021-11-27 17:03:25 +0100

    solution using middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,8 +14,5 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-	    if($this->app->environment(['production', 'stage'])) {
-		    URL::forceScheme('https');
-	    }
     }
 }
