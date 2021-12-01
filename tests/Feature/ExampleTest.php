037ac5fab0df839626db662ada8b8e7938a4ffe6
Adrian <322735+adrian-schnell@users.noreply.github.com>
2021-12-01 18:29:47 +0100

    add example test

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
