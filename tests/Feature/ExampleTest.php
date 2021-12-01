aba5ad58ed02d097eb97a11268207ad019762b32
Adrian Schnell <322735+adrian-schnell@users.noreply.github.com>
2021-12-01 18:31:20 +0100

    Test Setup (#42)
    * add test setup
    
    * remove demo tests
    
    * add example test
    
    * add example test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,15 +7,8 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    public function test_example(): void
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->assertTrue(true);
     }
 }
