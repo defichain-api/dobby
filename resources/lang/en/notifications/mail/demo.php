235623e27ce3b35b2be1acd7221930bfbb0070d1
Adrian <322735+adrian-schnell@users.noreply.github.com>
2021-12-06 00:43:10 +0100

    add demo notification

---
+++ resources/lang/en/notifications/mail/demo.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'subject'  => 'DeFiChain Dobby: TEST notification',
+	'greeting' => 'Hey friend!',
+	'message'  => "Dobby knows how to reach you by mail. If not already done, you can setup your notifications now on the link below",
+];
---
+++ resources/lang/en/notifications/telegram/demo.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ *
+ */
+return [
+	'message' => "Yeah *DOBBY* knows how to reach you on Telegram. If not already done, you can setup your notifications now on the link below",
+	'button'  => 'manage your notifications',
+];
