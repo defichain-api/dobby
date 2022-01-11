26956557bb583651d73023b8a224af4eca28c3a4
Adrian Schnell <322735+adrian-schnell@users.noreply.github.com>
2022-01-11 17:33:25 +0100

    Update summary command (#139)
    * command runs hourly now
    
    * update command to use the config
    
    * update mail notification wording

--- app/Console/Commands/CurrentSummaryNotificationCommand.php
+++ app/Console/Commands/CurrentSummaryNotificationCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Enum\SummaryInterval;
 use App\Models\User;
 use App\Notifications\CurrentSummaryTriggerNotification;
 use Illuminate\Console\Command;
@@ -9,12 +10,32 @@ use Illuminate\Console\Command;
 class CurrentSummaryNotificationCommand extends Command
 {
 	protected $signature = 'notification:current-summary';
-	protected $description = 'Send a daily summary to all users';
+	protected $description = 'Send a summary to all users with preset config';
 
 	public function handle()
 	{
 		User::withCount('gateways')
 			->having('gateways_count', '>', 0)
+			->whereHas('setting', function ($query) {
+				match (now()->hour) {
+					8 => $query->whereIn('summary_interval', [
+						SummaryInterval::DAILY_ONCE,
+						SummaryInterval::DAILY_TWICE,
+						SummaryInterval::DAILY_THRICE,
+						SummaryInterval::HOURLY,
+					]),
+					13 => $query->whereIn('summary_interval', [
+						SummaryInterval::DAILY_THRICE,
+						SummaryInterval::HOURLY,
+					]),
+					20 => $query->whereIn('summary_interval', [
+						SummaryInterval::DAILY_TWICE,
+						SummaryInterval::DAILY_THRICE,
+						SummaryInterval::HOURLY,
+					]),
+					default => $query->where('summary_interval', SummaryInterval::HOURLY)
+				};
+			})
 			->chunk(100, function ($users) {
 				$users->each(function (User $user) {
 					if (is_null($user->vaults->first())) {
