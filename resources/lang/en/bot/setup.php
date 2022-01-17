4ea1a9dfa2807bcc418566eba54e8d04a4a4aaa1
Adrian <322735+adrian-schnell@users.noreply.github.com>
2022-01-17 16:33:22 +0100

    fix issue on reconnecting telegram

--- resources/lang/en/bot/setup.php
+++ resources/lang/en/bot/setup.php
@@ -3,11 +3,12 @@
 return [
 	'enter_user_key'     => "*Dobby* 🧦 wants to help you.\r\n\r\nTherefore he needs to know how to reach you. - Please paste your *user key* now below. \r\n\r\nYou'll find it on :url",
 	'not_registered'     => "🔍\r\n*Dobby* 🧦 wants to help you, but he can't find your account.\r\nPlease register on :url \r\nand enter your *user key* afterwards:",
-	'already_registered' => "*Dobby* 🧦 already knows how to help you. Lean back and let him work for you.",
+	'already_registered' => "*Dobby* 🧦 already knows how to help you - everything is setup correctly. Lean back and let him work for you.",
+	'telegram_reconnect' => "*Dobby* 🧦 reconnected the telegram connection to your new account. Lean back and let him work for you.",
 
 	'registering' => [
 		'collect_data'   => "*Dobby* is busy doing magic(al?) things, please give him some seconds...",
-		'setup_finished' => "Now *Dobby* knows how to reach you.\r\n\r\nGo back to Dobby on :url and finish your setup.",
+		'setup_finished' => "Now *Dobby* knows how to reach you. Everything is setup correctly!\r\n\r\nGo back to Dobby on :url and finish your setup.",
 	],
 
 	'unknown_user' => "*Dobby* has problems to recognize who you are. Please set up your telegram account at :url",
