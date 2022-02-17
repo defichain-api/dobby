29338492263271bdfdf81829f95d96b3309d9811
Adrian Schnell <322735+adrian-schnell@users.noreply.github.com>
2022-02-17 15:59:33 +0100

    Remove WebApp (#153)
    * remove route for the webapp
    
    * remove webapp assets
    
    * cleanup public folder
    
    * remove UI assets

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use App\Http\Controllers\BotController;
-use App\Http\Controllers\WebAppController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', [WebAppController::class, 'index']);
 
 Route::match(['get', 'post'], 'telegram-bot', [BotController::class, 'handle']);
