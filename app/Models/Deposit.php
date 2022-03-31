dc4838216f8060cd1b073da81d560df8cdf088ed
Adrian <322735+adrian-schnell@users.noreply.github.com>
2022-03-31 17:03:25 +0200

    adopt migration changes on the model

--- app/Models/Deposit.php
+++ app/Models/Deposit.php
@@ -5,25 +5,21 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * @mixin \Eloquent
  * @property User   user
- * @property string userId
  * @property string txid
  * @property string senderAddress
  * @property int    block
  * @property float  amountDfi
- * @property Carbon created_at
- * @property Carbon updated_at
+ * @property Carbon received_at
  */
 class Deposit extends Model
 {
 	use HasFactory;
 
 	public $fillable = [
-		'userId',
 		'txid',
 		'senderAddress',
 		'block',
@@ -31,11 +27,10 @@ class Deposit extends Model
 	];
 	public $hidden = [
 		'id',
-		'updated_at',
 	];
 
-	public function user(): BelongsTo
+	public function user(): ?User
 	{
-		return $this->belongsTo(User::class, 'userId', 'id');
+		return UserSetting::where('depositFromAddress', $this->senderAddress)?->first()->user;
 	}
 }
