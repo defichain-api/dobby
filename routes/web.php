e366eb56f967d2eb058b4b384c24763ce46a251c
Adrian <322735+adrian-schnell@users.noreply.github.com>
2022-03-31 10:48:28 +0200

    add webhook url

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Api\Controller\TwilioWebhookController;
 use App\Http\Controllers\BotController;
 use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WebController::class, 'index']);
 Route::match(['get', 'post'], 'telegram-bot', [BotController::class, 'handle']);
+
+Route::post('twilio/webhook', TwilioWebhookController::class)
+	->middleware('twilio_webhook')
+	->name('webhook-twilio');
