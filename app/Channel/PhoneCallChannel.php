95fb298d760d20b318fa736df4bcf401a6df19e5
Adrian <322735+adrian-schnell@users.noreply.github.com>
2022-03-31 10:49:33 +0200

    add twilio phone number verification & reformatting

--- app/Channel/PhoneCallChannel.php
+++ app/Channel/PhoneCallChannel.php
@@ -17,16 +17,28 @@ class PhoneCallChannel
 		$this->twilioClient = new Client(config('twilio.account_sid'), config('twilio.auth_token'));
 	}
 
-	public function validatePhoneNumber(string $phoneNumber)
+	public function validatePhoneNumber(string $phoneNumber, string $countryIso): array
 	{
 		try {
 			$result = $this->twilioClient->lookups->v1->phoneNumbers($phoneNumber)
-				->fetch(["countryCode" => "DE"]);
+				->fetch(['countryCode' => $countryIso])->toArray();
 		} catch (TwilioException $e) {
-			return $e;
+			return [
+				'isValid' => false,
+				'message' => $e->getMessage(),
+			];
 		}
 
-		return $result->toArray();
+		return [
+			'isValid'                   => true,
+			'countryCode'               => $result['countryCode'],
+			'nationalPhoneNumberFormat' => $result['nationalFormat'],
+			'phoneNumber'               => $result['phoneNumber'],
+			'carrier'                   => [
+				'type' => $result['carrier']['type'] ?? 'n/a',
+				'name' => $result['carrier']['name'] ?? 'n/a',
+			],
+		];
 	}
 
 	public function initiateCall(): bool
@@ -36,11 +48,14 @@ class PhoneCallChannel
 				->executions
 				->create('+4991239988404', config('twilio.phone_number'), [
 					'parameters' => json_encode([
+						'retry_count'   => 0, // set to 0 as it's the initial call
 						'current_ratio' => 214,
 						'next_ratio'    => 180,
-						'dobby_key'     => 'Pariatur',
+						'dobby_key'     => '3280e083-5f56-49d8-80b9-a96933002120',
+						'vault_id'      => '00307d6cd3bb2b888304f22cce7c93ae64a7c4f30487d92648f2155a13e293ee',
 						'vault_name'    => 'Mein Vault',
 						'language'      => 'de',
+						'webhook_url'   => route('webhook-twilio'),
 					]),
 				]);
 
