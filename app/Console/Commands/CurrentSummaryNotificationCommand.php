ade31feabe6518e7dadaad40420bcb2f0709bb9f
Adrian <322735+adrian-schnell@users.noreply.github.com>
2022-04-29 09:54:23 +0200

    update command to respect the user settings

--- app/Console/Commands/CurrentSummaryNotificationCommand.php
+++ app/Console/Commands/CurrentSummaryNotificationCommand.php
@@ -14,35 +14,50 @@ class CurrentSummaryNotificationCommand extends Command
 
 	public function handle()
 	{
-		User::withCount('gateways')
-			->having('gateways_count', '>', 0)
-			->whereHas('setting', function ($query) {
-				match (now()->hour) {
-					8 => $query->whereIn('summary_interval', [
-						SummaryInterval::DAILY_ONCE,
-						SummaryInterval::DAILY_TWICE,
-						SummaryInterval::DAILY_THRICE,
-						SummaryInterval::HOURLY,
-					]),
-					13 => $query->whereIn('summary_interval', [
-						SummaryInterval::DAILY_THRICE,
-						SummaryInterval::HOURLY,
-					]),
-					20 => $query->whereIn('summary_interval', [
-						SummaryInterval::DAILY_TWICE,
-						SummaryInterval::DAILY_THRICE,
-						SummaryInterval::HOURLY,
-					]),
-					default => $query->where('summary_interval', SummaryInterval::HOURLY)
-				};
+		User::withCount('gateways') // select only users with active gateways
+		->having('gateways_count', '>', 0)
+			->withCount('vaults') // select only active users with a vault
+			->having('vaults_count', '>', 0)
+			->whereHas('setting', function ($query) { // select only users receiving the summary
+				return $query->whereIn('summary_interval', [
+					SummaryInterval::DAILY_ONCE,
+					SummaryInterval::DAILY_TWICE,
+					SummaryInterval::DAILY_THRICE,
+					SummaryInterval::HOURLY,
+				]);
 			})
 			->chunk(100, function ($users) {
 				$users->each(function (User $user) {
-					if (is_null($user->vaults->first())) {
-						return;
-					}
-					$user->notify(new CurrentSummaryTriggerNotification($user->vaults->first()));
+					$userSummaryInterval = $user->setting->summary_interval;
+					$this->setUsersTimezone($user);
+
+					$sendMessage = match (now()->hour) {
+						8 => in_array($userSummaryInterval, [
+							SummaryInterval::DAILY_ONCE,
+							SummaryInterval::DAILY_TWICE,
+							SummaryInterval::DAILY_THRICE,
+							SummaryInterval::HOURLY,
+						]),
+						13 => in_array($userSummaryInterval, [
+							SummaryInterval::DAILY_THRICE,
+							SummaryInterval::HOURLY,
+						]),
+						20 => in_array($userSummaryInterval, [
+							SummaryInterval::DAILY_TWICE,
+							SummaryInterval::DAILY_THRICE,
+							SummaryInterval::HOURLY,
+						]),
+						default => $userSummaryInterval == SummaryInterval::HOURLY
+					};
+
+					$sendMessage
+					&& $user->notify(new CurrentSummaryTriggerNotification($user->vaults->first()));
 				});
 			});
+	}
+
+	protected function setUsersTimezone(User $user): void
+	{
+		date_default_timezone_set($user->setting->timezone ?? config('app.timezone'));
 	}
 }
