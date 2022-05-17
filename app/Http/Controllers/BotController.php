a8d840be7346314c8dd42c62cf91f17f0fbe051e
Adrian <322735+adrian-schnell@users.noreply.github.com>
2022-05-17 21:58:34 +0200

    add command to main controller

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Api\Service\NotificationGatewayService;
 use App\Http\BotmanConversation\DisableGatewayConversation;
+use App\Http\BotmanConversation\RemoveSnoozeConversation;
 use App\Http\BotmanConversation\SetupConversation;
 use App\Http\BotmanConversation\SnoozeConversation;
 use App\Http\BotmanConversation\StateConversation;
@@ -29,13 +30,16 @@ class BotController
 		$botMan->hears('snooze_([0-9]+_[0-9]+)', function (BotMan $botMan) use ($message) {
 			$botMan->startConversation(new SnoozeConversation($message));
 		});
-		$botMan->hears('/vault_state', function (BotMan $botMan){
+		$botMan->hears('/vault_state', function (BotMan $botMan) {
 			$botMan->startConversation(new StateConversation());
 		});
-		$botMan->hears('/user_key', function (BotMan $botMan){
+		$botMan->hears('/user_key', function (BotMan $botMan) {
 			$botMan->startConversation(new UserKeyConversation());
 		});
-		$botMan->hears('/disable_telegram', function (BotMan $botMan){
+		$botMan->hears('/remove_snooze', function (BotMan $botMan) {
+			$botMan->startConversation(new RemoveSnoozeConversation());
+		});
+		$botMan->hears('/disable_telegram', function (BotMan $botMan) {
 			$botMan->startConversation(new DisableGatewayConversation(app(NotificationGatewayService::class)));
 		});
 		$botMan->fallback(function (Botman $botMan) {
