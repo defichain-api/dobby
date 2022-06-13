ee001fadb90a34bcd644b614b88808ae14d17106
Adrian <322735+adrian-schnell@users.noreply.github.com>
2022-06-13 21:43:50 +0200

    add mail

---
+++ app/Mail/LowAccountBalanceMail.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class LowAccountBalanceMail extends Mailable implements ShouldQueue
+{
+	use Queueable, SerializesModels;
+
+	public function __construct(protected User $user, protected float $accountBalance)
+	{
+	}
+
+	public function build()
+	{
+		$callCosts = (float)config('twilio.phone_call_cost');
+		$phoneCallAmount = floor($this->accountBalance / $callCosts);
+		ray([
+			'balance'    => $this->accountBalance,
+			'callCosts'  => $callCosts,
+			'callAmount' => $phoneCallAmount,
+		]);
+		$this->markdown('mail.low-account-balance', [
+			'balance'         => $this->accountBalance,
+			'phoneCallAmount' => $phoneCallAmount,
+			'phoneCallCost'   => $callCosts,
+		])->subject(__('mail/low-account-balance.subject', ['phoneCallAmount' => $phoneCallAmount]));
+	}
+}
---
+++ resources/lang/en/mail/low-account-balance.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'subject'  => 'Dobby: Your account balance is low, :phoneCallAmount calls left',
+	'title'    => 'Hurry up - low account balance',
+	'text'     => "Dobby took a look into the books and realized, that your account balance currently is :balance DFI. Thats enough for ***:phoneCallAmount more calls***.
+	
+To be sure Dobby can call you, you should go to the ***'Manage Phone Call'*** in Dobby (or link below) and deposit some more DFI.",
+	'btn_text' => 'Manage Phone Calls',
+];
