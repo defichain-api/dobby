93ef29fb981f513b6340bbafddaa28458bec518c
Adrian <322735+adrian-schnell@users.noreply.github.com>
2022-06-18 08:52:17 +0200

    implement new wording in mail

--- app/Mail/LowAccountBalanceMail.php
+++ app/Mail/LowAccountBalanceMail.php
@@ -12,19 +12,24 @@ class LowAccountBalanceMail extends Mailable implements ShouldQueue
 {
 	use Queueable, SerializesModels;
 
-	public function __construct(protected User $user, protected float $accountBalance)
-	{
+	public function __construct(
+		protected User  $user,
+		protected float $accountBalance,
+		protected int   $recommendedMinCallAmount
+	) {
 	}
 
 	public function build()
 	{
-		$callCosts = (float)config('twilio.phone_call_cost');
+		$callCosts = (float) config('twilio.phone_call_cost');
 		$phoneCallAmount = floor($this->accountBalance / $callCosts);
 
 		$this->markdown('mail.low-account-balance', [
-			'balance'         => $this->accountBalance,
-			'phoneCallAmount' => $phoneCallAmount,
-			'phoneCallCost'   => $callCosts,
+			'balance'                 => $this->accountBalance,
+			'phoneCallAmount'         => $phoneCallAmount,
+			'phoneCallCost'           => $callCosts,
+			'more_dfi'                => ($this->recommendedMinCallAmount * $callCosts) - $this->accountBalance,
+			'recommended_call_amount' => $this->recommendedMinCallAmount,
 		])->subject(__('mail/low-account-balance.subject', ['phoneCallAmount' => $phoneCallAmount]));
 	}
 }
