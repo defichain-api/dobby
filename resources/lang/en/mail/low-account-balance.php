c8bf6cae7dc976f5195806d9b034d685676bbe61
Adrian Schnell <322735+adrian-schnell@users.noreply.github.com>
2022-06-18 08:52:43 +0200

    Merge pull request #205
    * update wording
    
    * optimize command
    
    * implement new wording in mail

--- resources/lang/en/mail/low-account-balance.php
+++ resources/lang/en/mail/low-account-balance.php
@@ -5,6 +5,8 @@ return [
 	'title'    => 'Hurry up - low account balance',
 	'text'     => "Dobby took a look into the books and realized, that your account balance currently is ***:balance DFI***. Thats only ***:phoneCallAmount more calls***.
 	
+We highly recommend to fund :more_dfi DFI to receive a sum of ***:recommended_call_amount calls***.
+	
 To be sure Dobby can call you, you should go to the ***'Manage Phone Call'*** in Dobby (or link below) and deposit some more DFI.",
 	'btn_text' => 'Manage Phone Calls',
 ];
--- resources/views/mail/low-account-balance.blade.php
+++ resources/views/mail/low-account-balance.blade.php
@@ -1,7 +1,12 @@
 @component('mail::message')
 # {{ __('mail/low-account-balance.title') }}
 
-{{ __('mail/low-account-balance.text', ['balance' => $balance, 'phoneCallAmount' => $phoneCallAmount]) }}
+{{ __('mail/low-account-balance.text', [
+	'balance' => $balance,
+	'phoneCallAmount' => $phoneCallAmount,
+	'more_dfi' => $more_dfi,
+	'recommended_call_amount' => $recommended_call_amount,
+]) }}
 
 @component('mail::button', ['url' => config('app.url') . '/#/manage-phone-calls'])
 {{ __('mail/low-account-balance.btn_text') }}
