c7913877d6201f13647d1a8b1ea54a43fe475433
Adrian <322735+adrian-schnell@users.noreply.github.com>
2022-07-07 11:26:31 +0200

    wording fix

--- resources/lang/en/notifications/telegram/dusd_interest_rate.php
+++ resources/lang/en/notifications/telegram/dusd_interest_rate.php
@@ -2,5 +2,5 @@
 
 return [
 	'message' => "dUSD's interest rate has been automatically raised by DeFiChains stabilization mechanics to *:interest_rate %*.\r\n\r\nYour vaults containing a dUSD loan are:",
-	'single_vault_message' => "*:vault_name* [:vault_id](:vault_deeplink) - loan interest for dUSD :dusd_loan_interest %",
+	'single_vault_message' => "*:vault_name* [:vault_id](:vault_deeplink) - total loan interest for dUSD :dusd_loan_interest %",
 ];
