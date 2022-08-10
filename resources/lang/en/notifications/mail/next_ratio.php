771f4cdf92adc3be2d81a2068d7147956be392e4
Adrian <322735+adrian-schnell@users.noreply.github.com>
2022-08-10 17:50:48 +0200

    fix typo

--- resources/lang/en/notifications/mail/next_ratio.php
+++ resources/lang/en/notifications/mail/next_ratio.php
@@ -3,5 +3,5 @@
 return [
 	'subject'            => 'SEE THE FUTURE: Vault may drop below trigger ratio',
 	'greeting'           => 'Hey friend!',
-	'message'            => "***Dobby*** took a look into the future with a crystal ball.\r\n\r\nYour vault ***:vault_name*** [:vault_id](:vault_deeplink) collateration ratio will be *:next_ratio %* soon.\r\n\r\nIn *:block_diff blocks* (*~:diff_min minutes left*) your vault will drop below the trigger ratio of :trigger_ratio.",
+	'message'            => "***Dobby*** took a look into the future with a crystal ball.\r\n\r\nYour vault ***:vault_name*** [:vault_id](:vault_deeplink) collateralization ratio will be *:next_ratio %* soon.\r\n\r\nIn *:block_diff blocks* (*~:diff_min minutes left*) your vault will drop below the trigger ratio of :trigger_ratio.",
 ];
